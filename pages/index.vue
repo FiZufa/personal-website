609edacb8a69b3187fa0788aff2f311ee788a92e
Fitria Zu <fi.zufa0612@example.com>
2025-01-21 12:36:59 +0800

    create home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,62 @@
 <template>
     <Header />
 
+    <div class="body">
+
+        <div class="left">
+            <div class="left-content">
+                <img class="profile-img">
+
+                <p>Contact: fitria.zusni06@gmail.com</p>
+                <div class="social-media"></div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="right-content">
+                <h1>Fitria Zusni Farida</h1>
+                <p>(she/her)</p>
+
+                <p>Hi there! 👋</p>
+                <p>I'm a final-year student majoring in Computer Science and Engineering at Southern University of Science and Technology (SUSTech/南方科技大学), China. </p>
+                <p>Currently, my interests lie at the intersection of technology, data, and design. </p>
+                <p>Specificaly:</p>
+                <p>
+                    <ul>
+                        <li>turning data into actionable insights and building machine learning models</li>
+                        <li>solving problems through user-centered design to create usable and intuitive experiences</li>
+                        <li>building deliverable software systems and exploring software engineering concepts.</li>
+                    </ul>
+                </p>
+                <p>I am expected to graduate with my bachelor’s degree in June 2025, and <b>I’m actively seeking internship or full-time opportunities as Data Analyst, Data Scientist, or AI/Software Engineer 2025!</b> 🚀</p>
+            </div>
+
+        </div>
+
+    </div>
+
 </template>
 <script>
 import Header from '@/components/Header.vue'
 
 </script>
 <style>
+
+html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: 'Lato', sans-serif;
+    justify-content: center;
+    align-items: center;
+}
+
+.body {
+    margin: 32px 180px;
+    height: 550px;
+    width: 1080px;
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: sp;
+}
 
 </style>
