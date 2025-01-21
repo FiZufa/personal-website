5e96e9c473c00228d43d6bef28dc52ab37f8ab32
Fitria Zu <fi.zufa0612@example.com>
2025-01-21 21:27:19 +0800

    add porto vue

---
+++ components/PortoCourse.vue
@@ -0,0 +1,34 @@
+<template>
+    <h3>🎓Relavant Undergraduate Coursework Projects</h3>
+
+    <div class="porto-section">
+        
+        <div class="projects">
+            <p>title</p>
+            <div class="tags">
+                <p>tags</p>
+            </div>
+            <p>description</p>
+            <p>course name</p>
+            <div class="buttons">buttons</div>
+        </div>
+    </div>
+</template>
+<script>
+import { Divider } from '#components';
+import axios from "axios";
+</script>
+<style>
+html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+.porto-section {
+
+}
+
+</style>
--- pages/portofolio/index.vue
+++ pages/portofolio/index.vue
@@ -1,11 +1,28 @@
 <template>
     <Header />
+    <div class="porto-body">
+        <h1>Portofolio</h1>
+        <PortoCourse />
+    </div>
+    
 
 </template>
 <script>
 import Header from '@/components/Header.vue'
+import { PortoCourse } from '#components';
 
 </script>
 <style>
+html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+.porto-body {
+    margin: 0 180px;
+}
 
 </style>
